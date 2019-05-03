d08b3ebb4f67008a29ca2f194f079f4fa31332fc
MenglongLu <lumenglong2017@gmail.com>
2019-05-03 15:43:45 +0800

    清除了无效注释，且能正常读取数据

--- torch_model/Main_BU_RvNN.py
+++ torch_model/Main_BU_RvNN.py
@@ -8,15 +8,11 @@
 @time: Jan 24, 2018
 """
 
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
-
 import os
+import sys
+
 import BU_RvNN
 
-import theano
-from theano import tensor as T
 import numpy as np
 from numpy.testing import assert_array_almost_equal
 
@@ -24,7 +20,6 @@ import time
 import datetime
 import random
 from evaluate import *
-#from Util import *
 
 obj = "Twitter15" # choose dataset, you can choose either "Twitter15" or "Twitter16"
 fold = "3" # fold index, choose from 0-4
@@ -36,16 +31,12 @@ Nepoch = 500
 lr = 0.005
 
 unit="BU_RvNN-"+obj+str(fold)+'-vol.'+str(vocabulary_size)+tag
-#lossPath = "../loss/loss-"+unit+".txt"
-#modelPath = "../param/param-"+unit+".npz" 
 
 treePath = '../resource/data.BU_RvNN.vol_'+str(vocabulary_size)+tag+'.txt' 
 
 trainPath = "../nfold/RNNtrainSet_"+obj+str(fold)+"_tree.txt" 
 testPath = "../nfold/RNNtestSet_"+obj+str(fold)+"_tree.txt"
 labelPath = "../resource/"+obj+"_label_All.txt"
-
-#floss = open(lossPath, 'a+')
 
 ################################### tools #####################################
 def str2matrix(Str, MaxL): # str = index:wordfreq index:wordfreq
@@ -57,9 +48,7 @@ def str2matrix(Str, MaxL): # str = index:wordfreq index:wordfreq
         l += 1
     ladd = [ 0 for i in range( MaxL-l ) ]
     wordFreq += ladd 
-    wordIndex += ladd 
-    #print MaxL, l, len(Str.split(' ')), len(wordFreq)
-    #print Str.split(' ')
+    wordIndex += ladd
     return wordFreq, wordIndex 
 
 def loadLabel(label, l1, l2, l3, l4):
@@ -79,11 +68,10 @@ def loadLabel(label, l1, l2, l3, l4):
     return y_train, l1,l2,l3,l4
 
 def constructTree(tree):
-    ## tree: {index1:{'parent':, 'maxL':, 'vec':}
     ## 1. ini tree node
     index2node = {}
     for i in tree:
-        node = tree_gru.Node_tweet(idx=i)
+        node = BU_RvNN.Node_tweet(idx=i)
         index2node[i] = node
     ## 2. construct tree
     for j in tree:
@@ -91,7 +79,6 @@ def constructTree(tree):
         indexP = tree[j]['parent']
         nodeC = index2node[indexC]
         wordFreq, wordIndex = str2matrix( tree[j]['vec'], tree[j]['maxL'] )
-        #print tree[j]['maxL']
         nodeC.index = wordIndex
         nodeC.word = wordFreq
         ## not root node ## 
@@ -104,60 +91,59 @@ def constructTree(tree):
            root = nodeC
     ## 3. convert tree to DNN input    
     degree = tree[j]['max_degree']   
-    x_word, x_index, tree = tree_gru.gen_nn_inputs(root, max_degree=degree, only_leaves_have_vals=False)    
+    x_word, x_index, tree = BU_RvNN.gen_nn_inputs(root, max_degree=degree, only_leaves_have_vals=False)
     return x_word, x_index, tree       
                
 ################################# loas data ###################################
 def loadData():
-    print "loading tree label",
+    print("loading tree label",)
     labelDic = {}
     for line in open(labelPath):
         line = line.rstrip()
         label, eid = line.split('\t')[0], line.split('\t')[2]
         labelDic[eid] = label.lower()   
-    print len(labelDic)
+    print(len(labelDic))
     
-    print "reading tree", ## X
+    print("reading tree") ## X
     treeDic = {}
     for line in open(treePath):
         line = line.rstrip()
         eid, indexP, indexC = line.split('\t')[0], line.split('\t')[1], int(line.split('\t')[2])
-        max_degree, maxL, Vec = int(line.split('\t')[3]), int(line.split('\t')[4]), line.split('\t')[5]       
-        if not treeDic.has_key(eid):
+        max_degree, maxL, Vec = int(line.split('\t')[3]), int(line.split('\t')[4]), line.split('\t')[5]
+
+        if not treeDic.__contains__(eid):
            treeDic[eid] = {}
         treeDic[eid][indexC] = {'parent':indexP, 'max_degree':max_degree, 'maxL':maxL, 'vec':Vec}   
-    print 'tree no:', len(treeDic)
+    print('tree no:', len(treeDic))
     
-    print "loading train set", 
+    print("loading train set", )
     tree_train, word_train, index_train, y_train, c = [], [], [], [], 0
     l1,l2,l3,l4 = 0,0,0,0
     for eid in open(trainPath):
-        #if c > 8: break
         eid = eid.rstrip()
-        if not labelDic.has_key(eid): continue
-        if not treeDic.has_key(eid): continue 
+        if not labelDic.__contains__(eid): continue
+        if not treeDic.__contains__(eid): continue 
         if len(treeDic[eid]) < 2: continue
         ## 1. load label
         label = labelDic[eid]
         y, l1,l2,l3,l4 = loadLabel(label, l1, l2, l3, l4)
         y_train.append(y)
         ## 2. construct tree
-        #print eid
         x_word, x_index, tree = constructTree(treeDic[eid])
         tree_train.append(tree)
         word_train.append(x_word)
         index_train.append(x_index)
         c += 1
-    print l1,l2,l3,l4
+    print(l1,l2,l3,l4)
     
-    print "loading test set", 
+    print("loading test set", )
     tree_test,  word_test, index_test, y_test, c = [], [], [], [], 0
     l1,l2,l3,l4 = 0,0,0,0
     for eid in open(testPath):
         #if c > 4: break
         eid = eid.rstrip()
-        if not labelDic.has_key(eid): continue
-        if not treeDic.has_key(eid): continue 
+        if not labelDic.__contains__(eid): continue
+        if not treeDic.__contains__(eid): continue 
         if len(treeDic[eid]) < 2: continue        
         ## 1. load label        
         label = labelDic[eid]
@@ -169,12 +155,11 @@ def loadData():
         word_test.append(x_word)  
         index_test.append(x_index)  
         c += 1
-    print l1,l2,l3,l4
-    print "train no:", len(tree_train), len(word_train), len(index_train),len(y_train) 
-    print "test no:", len(tree_test), len(word_test), len(index_test), len(y_test)
-    print "dim1 for 0:", len(tree_train[0]), len(word_train[0]), len(index_train[0])
-    print "case 0:", tree_train[0][0], word_train[0][0], index_train[0][0]
-    #exit(0)
+    print(l1,l2,l3,l4)
+    print("train no:", len(tree_train), len(word_train), len(index_train),len(y_train) )
+    print("test no:", len(tree_test), len(word_test), len(index_test), len(y_test))
+    print("dim1 for 0:", len(tree_train[0]), len(word_train[0]), len(index_train[0]))
+    print("case 0:", tree_train[0][0], word_train[0][0], index_train[0][0])
     return tree_train, word_train, index_train, y_train, tree_test, word_test, index_test, y_test
 
 ##################################### MAIN ####################################        
@@ -185,17 +170,9 @@ tree_train, word_train, index_train, y_train, tree_test, word_test, index_test, 
 t0 = time.time()
 model = BU_RvNN.RvNN(vocabulary_size, hidden_dim, Nclass)
 t1 = time.time()
-print 'Recursive model established,', (t1-t0)/60
-
-#if os.path.isfile(modelPath):
-#   load_model_Recursive_gruEmb(modelPath, model) 
+print('Recursive model established,', (t1-t0)/60)
+sys.exit(0)
 ######debug here######
-#print len(tree_test[121]), len(index_test[121]), len(word_test[121])
-#print tree_test[121]
-#exit(0)
-#loss, pred_y = model.train_step_up(word_test[121], index_test[121], tree_test[121], y_test[121], lr)
-#print loss, pred_y
-#exit(0)
 ######################
 
 ## 3. looping SGD
@@ -206,41 +183,29 @@ for epoch in range(Nepoch):
     indexs = [i for i in range(len(y_train))]
     random.shuffle(indexs) 
     for i in indexs:
-        #print i,
         loss, pred_y = model.train_step_up(word_train[i], index_train[i], tree_train[i], y_train[i], lr)
-        #print loss, pred_y
         losses.append(loss)
         num_examples_seen += 1
-    print "epoch=%d: loss=%f" % ( epoch, np.mean(losses) )
-    #floss.write(str(time)+": epoch="+str(epoch)+" loss="+str(loss) +'\n')
+    print("epoch=%d: loss=%f" % ( epoch, np.mean(losses) ))
     sys.stdout.flush()
     
     ## cal loss & evaluate
     if epoch % 5 == 0:
        losses_5.append((num_examples_seen, np.mean(losses))) 
        time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-       print "%s: Loss after num_examples_seen=%d epoch=%d: %f" % (time, num_examples_seen, epoch, np.mean(losses))
-       #floss.write(str(time)+": epoch="+str(epoch)+" loss="+str(loss) +'\n') 
-       #floss.flush()        
+       print("%s: Loss after num_examples_seen=%d epoch=%d: %f" % (time, num_examples_seen, epoch, np.mean(losses)))
        sys.stdout.flush()
        prediction = []
        for j in range(len(y_test)):
-           #print j
            prediction.append(model.predict_up(word_test[j], index_test[j], tree_test[j]) )   
        res = evaluation_4class(prediction, y_test) 
-       print 'results:', res
-       #floss.write(str(res)+'\n')
-       #floss.flush() 
+       print('results:', res)
        sys.stdout.flush()
        ## Adjust the learning rate if loss increases
        if len(losses_5) > 1 and losses_5[-1][1] > losses_5[-2][1]:
           lr = lr * 0.5   
-          print "Setting learning rate to %f" % lr
-          #floss.write("Setting learning rate to:"+str(lr)+'\n')
-          #floss.flush() 
+          print("Setting learning rate to %f" % lr)
           sys.stdout.flush()
-       #save_model_Recursive_gruEmb(modelPath, model)   
     sys.stdout.flush()
     losses = []
     
-#floss.close()    
--- torch_model/evaluate.py
+++ torch_model/evaluate.py
@@ -8,9 +8,6 @@
 """
 
 import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
-
 import random
 import os
 import re
