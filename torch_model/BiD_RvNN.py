8bc027a98bd1ed90c09e1aed6250be9461f99396
LuMelon <lumenglong2017@gmail.com>
2019-05-11 18:00:46 +0800

    2 full layers

--- torch_model/BiD_RvNN.py
+++ torch_model/BiD_RvNN.py
@@ -57,9 +57,9 @@ class RvNN(nn.Module):
         bu_final_state = self.bu_compute_tree_states(bu_x_word, bu_x_index, bu_tree)
         final_state = torch.cat((td_final_state, bu_final_state), dim=0)
         final_state1 = self.W_out1.mul(final_state).sum(dim=1) +self.b_out1
-        final_state2 = self.W_out2.mul(final_state1).sum(dim=1) + self.b_out2
+        # final_state2 = self.W_out2.mul(final_state1).sum(dim=1) + self.b_out2
         # final_state3 = self.W_out3.mul(final_state2).sum(dim=1) + self.b_out3
-        pred, loss = self.predAndLoss(final_state2, y)
+        pred, loss = self.predAndLoss(final_state1, y)
         return pred, loss
 
     def td_recursive_unit(self, child_word, child_index, parent_h):
@@ -137,6 +137,6 @@ class RvNN(nn.Module):
         bu_final_state = self.bu_compute_tree_states(bu_x_word, bu_x_index, bu_tree)
         final_state = torch.cat((td_final_state, bu_final_state), dim=0)
         final_state1 = self.W_out1.mul(final_state).sum(dim=1) +self.b_out1
-        final_state2 = self.W_out2.mul(final_state1).sum(dim=1) + self.b_out2
+        # final_state2 = self.W_out2.mul(final_state1).sum(dim=1) + self.b_out2
         # final_state3 = self.W_out3.mul(final_state2).sum(dim=1) + self.b_out3
-        return F.softmax(self.W_out4.mul(final_state2).sum(dim=1) +self.b_out4)
+        return F.softmax(self.W_out4.mul(final_state1).sum(dim=1) +self.b_out4)
