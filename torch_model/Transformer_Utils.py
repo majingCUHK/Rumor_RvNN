d042a0efa886a766687f7bb586313ec70954027d
MelonLu <lumenglong2017@gmail.com>
2019-06-10 21:18:23 +0800

    对标准Transformer的代码进行了一个注释

--- torch_model/Transformer_Utils.py
+++ torch_model/Transformer_Utils.py
@@ -137,3 +137,17 @@ class DecoderLayer(nn.Module):
         x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
         x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
         return self.sublayer[2](x, self.feed_forward)
+
+
+class Decoder(nn.Module):
+    "Generic N layer decoder with masking."
+
+    def __init__(self, layer, N):
+        super(Decoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, memory, src_mask, tgt_mask)
+        return self.norm(x)
