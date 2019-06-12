9f1b6ca94b59e729eae0382199340cef18bc1523
LuMelon <lumenglong2017@gmail.com>
2019-06-12 11:46:07 +0800

    解码层中，对自身进行注意力调整时的矩阵变换问题

--- torch_model/Transformer_Utils.py
+++ torch_model/Transformer_Utils.py
@@ -152,7 +152,7 @@ class DecoderLayer(nn.Module):
     def forward(self, x, memory, src_mask, tgt_mask):
         "Follow Figure 1 (right) for connections."
         m = memory
-        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
+        x = self.sublayer[0](x, lambda x: self.self_attn(x.unsqueeze(0), x.unsqueeze(0), x.unsqueeze(0), tgt_mask))
         x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
         return self.sublayer[2](x, self.feed_forward)
 
